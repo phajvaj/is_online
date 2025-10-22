ff91df041d0d7a53e10bec621ab36d9800d11a34
phajvaj <saciw.doi@gmail.com>
2025-10-23 00:13:59 +0700

    Revert "nan hosxp xe pg"
    This reverts commit e73491f8c2ea0411177f23e3fb86a4ec8c9f098e.

--- src/routes/his/hismodel.ts
+++ src/routes/his/hismodel.ts
@@ -4,98 +4,94 @@
 // require('dotenv').config({ path: path.join(__dirname, '../config') });
 // require('dotenv').config({ path: '/config' });
 
-import { HisIHospitalModel } from "../../models/his/his_ihospital";
-import { HisThiadesModel } from "../../models/his/his_thiades";
-import { HisHosxpv3Model } from "../../models/his/his_hosxpv3";
-import { HisHosxpv4Model } from "../../models/his/his_hosxpv4";
-import { HisHosxpv4PgModel } from "../../models/his/his_hosxpv4pg";
-import { HisHosxpPcuModel } from "../../models/his/his_hosxppcu";
-import { HisJhcisModel } from "../../models/his/his_jhcis";
-import { HisMdModel } from "../../models/his/his_md";
-import { HisKpstatModel } from "../../models/his/his_kpstat";
-import { HisMkhospitalModel } from "../../models/his/his_mkhospital";
-import { HisModel } from "../../models/his/his";
-import { HisNemoModel } from "../../models/his/his_nemo";
-import { HisPmkModel } from "../../models/his/his_pmk";
-import { HisMyPcuModel } from "../../models/his/his_mypcu";
-import { HisEmrSoftModel } from "../../models/his/his_emrsoft";
-import { HisHaosModel } from "../../models/his/his_haos";
-import { HisSsbSriHModel } from "../../models/his/his_ssb_srih";
+import { HisIHospitalModel } from '../../models/his/his_ihospital';
+import { HisThiadesModel } from '../../models/his/his_thiades';
+import { HisHosxpv3Model } from '../../models/his/his_hosxpv3';
+import { HisHosxpv4Model } from '../../models/his/his_hosxpv4';
+import { HisHosxpPcuModel } from '../../models/his/his_hosxppcu';
+import { HisJhcisModel } from '../../models/his/his_jhcis';
+import { HisMdModel } from '../../models/his/his_md';
+import { HisKpstatModel } from '../../models/his/his_kpstat';
+import { HisMkhospitalModel } from '../../models/his/his_mkhospital';
+import { HisModel } from '../../models/his/his';
+import { HisNemoModel } from '../../models/his/his_nemo';
+import { HisPmkModel } from '../../models/his/his_pmk';
+import { HisMyPcuModel } from '../../models/his/his_mypcu';
+import { HisEmrSoftModel } from '../../models/his/his_emrsoft';
+import { HisHaosModel } from '../../models/his/his_haos';
+import { HisSsbSriHModel } from '../../models/his/his_ssb_srih';
 
-console.log("HIS Provider:", process.env.HIS_PROVIDER);
+console.log('HIS Provider:', process.env.HIS_PROVIDER);
 const hisProvider = process.env.HIS_PROVIDER.toLowerCase();
 
 let hisModel: any;
 switch (hisProvider) {
-    case "ihospital":
-    case "ezhosp":
-        hisModel = new HisIHospitalModel();
-        break;
-    case "thiades":
-        hisModel = new HisThiadesModel();
-        break;
-    case "hosxpv3":
-        hisModel = new HisHosxpv3Model();
-        break;
-    case "hosxpv4":
-        hisModel = new HisHosxpv4Model();
-        break;
-    case "hosxpv4pg":
-        hisModel = new HisHosxpv4PgModel();
-        break;
-    case "hosxppcu":
-        hisModel = new HisHosxpPcuModel();
-        break;
-    case "mkhospital":
-        hisModel = new HisMkhospitalModel();
-        break;
-    case "nemo":
-    case "nemo_refer":
-        hisModel = new HisNemoModel();
-        break;
-    case "ssb":
-        hisModel = new HisSsbSriHModel(); // SSB รพ.สระบุรี
-        break;
-    case "infod":
-    case "homc":
-        // hisModel = new HisInfodModel();
-        break;
-    case "hi":
-        // hisModel = new HisHiModel();
-        break;
-    case "himpro":
-        // hisModel = new HisHimproModel();
-        break;
-    case "jhcis":
-        hisModel = new HisJhcisModel();
-        break;
-    case "mypcu":
-        hisModel = new HisMyPcuModel();
-        break;
-    case "hospitalos":
-        // hisModel = new HisHospitalOsModel();
-        break;
-    case "jhos":
-        // hisModel = new HisJhosModel();
-        break;
-    case "pmk":
-        hisModel = new HisPmkModel();
-        break;
-    case "md":
-        hisModel = new HisMdModel();
-        break;
-    case "emrsoft":
-        hisModel = new HisEmrSoftModel();
-        break;
-    case "haos":
-        hisModel = new HisHaosModel();
-        break;
-    case "spdc":
-    case "kpstat":
-        hisModel = new HisKpstatModel();
-        break;
-    default:
-        hisModel = new HisModel();
+  case 'ihospital':
+  case 'ezhosp':
+    hisModel = new HisIHospitalModel();
+    break;
+  case 'thiades':
+    hisModel = new HisThiadesModel();
+    break;
+  case 'hosxpv3':
+    hisModel = new HisHosxpv3Model();
+    break;
+  case 'hosxpv4':
+    hisModel = new HisHosxpv4Model();
+    break;
+  case 'hosxppcu':
+    hisModel = new HisHosxpPcuModel();
+    break;
+  case 'mkhospital':
+    hisModel = new HisMkhospitalModel();
+    break;
+  case 'nemo':
+  case 'nemo_refer':
+    hisModel = new HisNemoModel();
+    break;
+  case 'ssb':
+    hisModel = new HisSsbSriHModel(); // SSB รพ.สระบุรี
+    break;
+  case 'infod':
+  case 'homc':
+    // hisModel = new HisInfodModel();
+    break;
+  case 'hi':
+    // hisModel = new HisHiModel();
+    break;
+  case 'himpro':
+    // hisModel = new HisHimproModel();
+    break;
+  case 'jhcis':
+    hisModel = new HisJhcisModel();
+    break;
+  case 'mypcu':
+    hisModel = new HisMyPcuModel();
+    break;
+  case 'hospitalos':
+    // hisModel = new HisHospitalOsModel();
+    break;
+  case 'jhos':
+    // hisModel = new HisJhosModel();
+    break;
+  case 'pmk':
+    hisModel = new HisPmkModel();
+    break;
+  case 'md':
+    hisModel = new HisMdModel();
+    break;
+  case 'emrsoft':
+    hisModel = new HisEmrSoftModel();
+    break;
+  case 'haos':
+    hisModel = new HisHaosModel();
+    break;
+  case 'spdc':
+  case 'kpstat':
+    hisModel = new HisKpstatModel();
+    break;
+  default:
+    hisModel = new HisModel();
 }
 
 export default hisModel;
